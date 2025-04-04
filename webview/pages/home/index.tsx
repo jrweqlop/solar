e26fe1cd84b3e964e3a6ceb4ebbc4b6f5ad19ea7
jrweqlop <herher.haha@windowslive.com>
2025-04-04 12:58:29 +0700

    update project solar
    1.update web

--- webview/pages/home/index.tsx
+++ webview/pages/home/index.tsx
@@ -7,12 +7,21 @@ import Container from '@mui/material/Container'
 import TimeClock from '../../src/shared/TimeClock'
 import ViewDataAllBCU from '../../src/modules/ViewDataAllBCU'
 import ProviderMain from '../../src/components/Provider/ProviderMain'
+import { useEffect } from 'react'
+
+const urlWs = process.env.NEXT_PUBLIC_WS_CONNECT || 'ws://localhost:7755/device'
 
 const Index = () => {
 
     const [thidData] = useAtom<InternetData[]>(DataWsJson)
 
-    const { readyState, nowData } = HookWebsocket('ws://localhost:81/device')
+    // const url = `ws://${urlWs}:81/device`
+
+    const { readyState, nowData } = HookWebsocket(urlWs)
+
+    useEffect(() => {
+        console.log(urlWs)
+    }, [])
 
     return (
         <>
